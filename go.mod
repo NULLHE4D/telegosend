c1fd36c43c81e01785061ea11f8f995b6cb882a8
NULLHE4D <adamlibya17@gmail.com>
2021-01-13 17:41:29 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/NULLHE4D/telegosend
+
+go 1.13
+
+require gopkg.in/tucnak/telebot.v2 v2.3.5 // indirect
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    _ "fmt"
+    "os"
+    "log"
+    "strconv"
+    "bufio"
+    "strings"
+
+    telebot "gopkg.in/tucnak/telebot.v2"
+)
+
+func main() {
+
+    botToken := os.Getenv("TGBOTTOKEN"); if len(botToken) == 0 {
+        log.Fatal("missing TGBOTTOKEN environment variable")
+    }
+
+    chatId, err := strconv.Atoi(os.Getenv("TGCHATID")); if err != nil {
+        log.Fatal("missing or invalid CHATID environmnet variable")
+    }
+
+    recipient := telebot.User{ID: chatId}
+
+    bot, err := telebot.NewBot(telebot.Settings{
+        Token:  botToken,
+        ParseMode: "Markdown",
+    }); if err != nil {
+        log.Fatal(err)
+    }
+
+    scanner := bufio.NewScanner(os.Stdin)
+    var sb strings.Builder
+    for scanner.Scan() {
+        sb.WriteString(scanner.Text())
+        sb.WriteString("\n")
+    }
+
+    _, err = bot.Send(&recipient, sb.String()); if err != nil {
+        log.Fatal(err)
+    }
+
+}
