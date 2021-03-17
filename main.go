9a2808c6a801c52de9dcb171ec8738c6b74755a7
NULLHE4D <adamlibya17@gmail.com>
2021-03-17 17:20:36 +0200

    using golang http client to communicate with telegram API instead of using telebot dependency

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
-    _ "fmt"
+    "fmt"
     "os"
     "log"
     "strconv"
+    "io/ioutil"
     "bufio"
     "strings"
-
-    telebot "gopkg.in/tucnak/telebot.v2"
+    "time"
+    "net/http"
+    "net/url"
 )
+
+func checkErr(err error) {
+    if err != nil {
+        urlErr := err.(*url.Error)
+        if urlErr.Timeout() {
+            log.Fatal("request timed out")
+        } else {
+            log.Fatal("something went wrong: ", err)
+        }
+    }
+}
+
+func checkOk(data []byte) {
+    if dataStr := string(data); strings.Contains(dataStr, "\"ok\":false") {
+        log.Fatal("something went wrong: ", dataStr)
+    }
+}
 
 func main() {
 
@@ -21,24 +40,34 @@ func main() {
         log.Fatal("missing or invalid TGCHATID environment variable")
     }
 
-    recipient := telebot.User{ID: chatId}
-
-    bot, err := telebot.NewBot(telebot.Settings{
-        Token:  botToken,
-        ParseMode: "Markdown",
-    }); if err != nil {
-        log.Fatal(err)
+    client := &http.Client {
+        Timeout: 10 * time.Second,
     }
 
     scanner := bufio.NewScanner(os.Stdin)
     var sb strings.Builder
+    scanner.Scan()
+    sb.WriteString(scanner.Text())
     for scanner.Scan() {
-        sb.WriteString(scanner.Text())
         sb.WriteString("\n")
+        sb.WriteString(scanner.Text())
     }
 
-    _, err = bot.Send(&recipient, sb.String()); if err != nil {
-        log.Fatal(err)
-    }
+    baseUrl := fmt.Sprintf("https://api.telegram.org/bot%s/", botToken)
+
+    getMeUrl := baseUrl + "getMe"
+    res, err := client.Get(getMeUrl)
+    checkErr(err)
+    data, _ := ioutil.ReadAll(res.Body)
+    res.Body.Close()
+    checkOk(data)
+
+    sendMessageUrl := baseUrl + "sendMessage"
+    reqBody := fmt.Sprintf("chat_id=%d&text=%s&parse_mode=MarkdownV2", chatId, sb.String())
+    res, err = client.Post(sendMessageUrl, "application/x-www-form-urlencoded", strings.NewReader(reqBody))
+    checkErr(err)
+    data, _ = ioutil.ReadAll(res.Body)
+    res.Body.Close()
+    checkOk(data)
 
 }
